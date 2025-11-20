dc87910205f26048961ba002831dd7f8e881e66c
luxen4 <superlaya51@gmail.com>
2025-11-20 06:03:05 +0100

    20/11/2025, Reformas2

--- estructura/header/enlaces-principales-generico.php
+++ estructura/header/enlaces-principales-generico.php
@@ -192,7 +192,7 @@
                         </li>
 
                         <li>
-                            <a href="/val-de-loire/coches-de-alquiler"
+                            <a href="/val-de-loire/coches-de-alquiler.php"
                                class="flex items-center gap-3 px-4 py-2 text-emerald-100 hover:bg-emerald-600">
                                 🚘 Coches de alquiler
                             </a>
