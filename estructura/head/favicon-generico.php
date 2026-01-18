e4ced1591d7ff8c300102c7b687a6d80ac701713
Luxen-4 <superlaya51@gmail.com>
2026-01-19 00:29:28 +0100

    19/1/2026. Creación del archivo: Guia Castillos 2

--- estructura/head/favicon-generico.php
+++ estructura/head/favicon-generico.php
@@ -1,6 +1,10 @@
   
   <!-- ✅ Favicon -->
-  <link rel="icon" href="<?= $web_site; ?>/assets/favicon.ico" sizes="any">
+  <?php /* <link rel="icon" href="<?= $web_site; ?>/assets/favicon.ico" sizes="any">
   <link rel="icon" type="image/png" sizes="32x32" href="<?= $web_site; ?>/assets/logo.png">
   <link rel="icon" type="image/png" sizes="16x16" href="<?= $web_site; ?>/assets/logo.png">
-  <link rel="apple-touch-icon" href="<?= $web_site; ?>/assets/logo.png">
+  <link rel="apple-touch-icon" href="<?= $web_site; ?>/assets/logo.png"> */ ?>
+
+
+<link rel="icon" href="/val-de-loire/assets/favicon.ico" type="image/x-icon">
+<link rel="shortcut icon" href="/val-de-loire/assets/favicon.ico" type="image/x-icon">
